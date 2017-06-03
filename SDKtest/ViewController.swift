00d16d5ee92c450d02001a174cd591c20e704391
Santiesteban <alvaro.alfaro@outlook.com>
2017-06-03 16:32:12 -0600

    Now the controller fetches the images from Facebook and updates the information on the second screen

--- SDKtest/ViewController.swift
+++ SDKtest/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
+import FBSDKShareKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    @IBOutlet weak var FBLoginButton: FBSDKLoginButton!
+    
+    var familyDetails: [[String:AnyObject]]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.FBLoginButton.delegate = self
+        self.FBLoginButton.readPermissions = ["public_profile", "user_relationships"]
+        self.FBLoginButton.loginBehavior = .systemAccount
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func testGraphRequest(_ sender: UIButton) {
+        returnUsersData(completion: {(result)->Void in
+            self.nextScreen()
+        })
     }
-
-
+    
+    func nextScreen() {
+        performSegue(withIdentifier: "segueTableView", sender: self)
+    }
+    
+    func returnUsersData(completion:@escaping (_ result:[String:AnyObject])->Void) {
+        
+        FBSDKGraphRequest(graphPath: "/me/family", parameters: ["fields": "name, relationship"]).start(completionHandler: {(connection, result, error) -> Void in
+            if let result = result as! [String:AnyObject]? {
+                if let familyDictionary = result["data"] as? [[String:AnyObject]]{
+                    self.familyDetails = familyDictionary
+                }
+                completion(result)
+            }
+        })
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let controller = segue.destination as! TestTableViewController
+        controller.familyDict = self.familyDetails
+        
+    }
+    
+    
 }
-
